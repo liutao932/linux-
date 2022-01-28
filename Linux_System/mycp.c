5439e3e47445440601b4f7a0cd7263cf49a35abf
liutao932 <1258571928@qq.com>
2022-01-28 23:18:17 +0800

    mycp
    Signed-off-by: liutao932 <1258571928@qq.com>

--- Linux_System/mycp.c
+++ Linux_System/mycp.c
@@ -1,34 +1,40 @@
- #include<stdio.h>
- #include<stdlib.h>
- int main(int argc,char *argv[])
- {
-         int ch;
-         FILE *ftb,*ftp;
-         if(argc <3)
-         {
-                 printf("uage%s :<srcfile> <desfile>\n",argv[0]);
-                 exit(1);
-         }
-         ftb = fopen(argv[1],"r");
-         if(ftb == NULL)
-         {
-                 perror("fopen()");
-                 exit(1);
-         }
-         ftp = fopen(argv[2],"w");
-         if(ftp ==  NULL)
-         {
-                 perror("fopen()");
-                 exit(1);
-         }
-         while(1)
-         {
-              ch = fgetc(ftb);
-              if(ch ==  EOF)
-                  break;
-              fputc(ch,ftp);
-         }
-         fclose(ftp);
-         fclose(ftb);
-         exit(0);
- }
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int main(int argc,char **argv)
+{
+
+	if(argc <3)
+	{
+		fprintf(stderr,"Useg:%s<src_file><dst_file>",argv[0]);
+		exit(-1);
+	}
+	int ch;
+	FILE * fbs, *fdb;
+	fbs = fopen(argv[1],"r");
+	if(fbs == NULL)
+	{
+		perror("fopen()");
+		exit(1);
+	}
+	fdb = fopen(argv[2],"w");
+	if(fdb == NULL)
+	{
+		fclose(fbs);
+		perror("fopen()");
+		exit(1);
+	}
+
+	while(1)
+	{
+		ch = fgetc(fbs);
+		if(ch == EOF)
+		{
+			break;
+		}
+		fputc(ch,fdb);
+	}
+	fclose(fbs);
+	fclose(fdb);
+}
