f15ac0800bb1a70c4dfdf7773ca58286162eef76
liutao932 <87967759+liutao932@users.noreply.github.com>
2022-02-09 18:06:06 +0800

    Create mytbf.h

---
+++ process/single/令牌桶/mytbf.h
@@ -0,0 +1,11 @@
+#ifndef _MYTBF_H_
+#define _MYTBF_H_
+#define MYTBF_MAX 1024
+typedef void mytbf_t;
+mytbf_t mytbf_init(int cps,int burst);
+int mytbf_fetchtoken(mytbf_t*,int);
+int mytbf_returntoken(mytbf_t*,int);
+int mytbf_destroy(mytbf_t *);
+
+
+#else
